89d0429fc69b2d2c04e71fe847ae8a009907f198
herbgrace <thelen.alexander05@gmail.com>
2025-12-01 13:19:35 -0700

    Cleaned up Day 1 code & added comments

--- Day1/CodeBreaker.py
+++ Day1/CodeBreaker.py
@@ -4,13 +4,15 @@ def firstChallenge():
     with open('Day1/input.txt', 'r') as f:
         content = f.readlines()
         for line in content:
+            # Determine how far/direction to turn
             turn = int(line[1:])
-            if(line.startswith('R')):
-                # print("Turning right ", turn)
-                current += turn
-            else:
-                # print("Turning left ", turn)
-                current -= turn
+            if(line.startswith('L')):
+                turn = -turn
+
+            # Turn the dial
+            current += turn
+
+            # Increment count if it's on 0
             if (current % 100 == 0):
                 count += 1
         print("The password is: ", count)
@@ -21,23 +23,22 @@ def secondChallenge():
     with open('Day1/input.txt', 'r') as f:
         content = f.readlines()
         for line in content:
+            # Determine how far/direction to turn
             startingNum = current
             turn = int(line[1:])
             if (line.startswith('L')):
                 turn = -turn
 
-            # print("Starting Number: ", startingNum)
-            # print("Number Turned: ", turn)
+            # Turn the dial
             current += turn
 
-            # print("Ending Number: ", current)
-            # print("Starting Count: ", count)
+            # Determine if 0 was crossed, then increment counter if so
             count += abs(int(current/100))
             if (current <= 0 and startingNum != 0):
                 count += 1
-            # print("Ending Count: ", count)
+
+            # Reset the dial to be in range
             current %= 100
-            # print("")
 
         print("The REAL password is: ", count)
 
